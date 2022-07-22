8986d1bbbe102d6135d7b3a24572f5c806c6ef57
raheelanwaar03 <raheelanwaar0316@gmail.com>
2022-07-22 12:09:02 +0500

    side bar changes +images

--- resources/views/user/user_layout/sideNav.blade.php
+++ resources/views/user/user_layout/sideNav.blade.php
@@ -1,32 +1,25 @@
 <!--Main Navigation-->
 <header>
     <!-- Sidebar -->
-    <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
+    <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-dark">
         <div class="position-sticky">
             <div class="list-group list-group-flush mx-3 mt-4">
                 <a href="#" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
-                    <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Buy E-currency</span>
+                    <i class="fa fa-credit-card-alt" aria-hidden="true"></i><span>Buy E-currency</span>
                 </a>
+                <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i class="fa fa-money"
+                        aria-hidden="true"></i><span>Sell E-currency
+                    </span></a>
                 <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i
-                        class="fas fa-lock fa-fw me-3"></i><span>Sell E-currency
-                        </span></a>
-                <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i
-                        class="fas fa-chart-line fa-fw me-3"></i><span>My WALLETS</span></a>
+                        class="fas fa-chart-line fa-fw me-3"></i><span>My WALLET</span></a>
                 <a href="#" class="list-group-item list-group-item-action py-2 ripple">
-                    <i class="fas fa-chart-pie fa-fw me-3"></i><span>Transaction History</span>
+                    <i class="fas fa-chart-pie fa-fw me-3"></i><span>History</span>
+                </a>
+                <a href="#" class="list-group-item list-group-item-action py-2 ripple">
+                    <i class="fas fa-chart-bar fa-fw me-3"></i><span>News</span>
                 </a>
                 <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i
-                        class="fas fa-chart-bar fa-fw me-3"></i><span>Logout</span></a>
-                <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i
-                        class="fas fa-globe fa-fw me-3"></i><span>International</span></a>
-                <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i
-                        class="fas fa-building fa-fw me-3"></i><span>Partners</span></a>
-                <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i
-                        class="fas fa-calendar fa-fw me-3"></i><span>Calendar</span></a>
-                <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i
-                        class="fas fa-users fa-fw me-3"></i><span>Users</span></a>
-                <a href="#" class="list-group-item list-group-item-action py-2 ripple"><i
-                        class="fas fa-money-bill fa-fw me-3"></i><span>Sales</span></a>
+                        class="fa fa-sign-out" aria-hidden="true"></i><span>Logout</span></a>
             </div>
         </div>
     </nav>
