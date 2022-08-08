6a6ccecd464e47a69dec7f9f28552d3262b9354a
raheelanwaar03 <raheelanwaar0316@gmail.com>
2022-08-08 13:19:48 +0500

    email link changed

--- resources/views/emails/user/completeTranction.blade.php
+++ resources/views/emails/user/completeTranction.blade.php
@@ -4,8 +4,8 @@
 Hi {{auth()->user()->name}}, your tranction is completed.
 Please check your account and visit our website for more Buying E-Currency.
 
-@component('mail::button', ['url' => '/user'])
-View Dashboard
+@component('mail::button', ['url' => '/user/review'])
+Give Feedback
 @endcomponent
 
 Thanks,<br>
