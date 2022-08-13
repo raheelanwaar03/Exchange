8716b88ad84fadb928297cd4d84ec691689f8348
raheelanwaar03 <raheelanwaar0316@gmail.com>
2022-08-13 11:29:51 +0500

    remove itmes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\Schema;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,7 +24,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Paginator::useBootstrap();
-        Schema::defaultStringLength(191);
+       //
     }
 }
