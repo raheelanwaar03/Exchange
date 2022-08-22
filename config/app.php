9dd14532df6897f98f1804c1f9c5e8f503c3e0ac
raheelanwaar03 <raheelanwaar0316@gmail.com>
2022-08-22 11:48:36 +0500

    class added

--- config/app.php
+++ config/app.php
@@ -194,7 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        "Buzz\LaravelGoogleCaptcha\CaptchaServiceProvider"
+        Buzz\LaravelGoogleCaptcha\CaptchaServiceProvider::class,
 
     ],
 
