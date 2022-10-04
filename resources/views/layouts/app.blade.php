db26ed7ffbbc82f8d4025490fed962af5319b04e
Raheel Anwaar <raheelanwaar0316@gmail.com>
2022-10-04 16:34:10 +0500

    now user will redirect to mobile app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 @yield('content')
 
 <div class="whatsapp">
-    <a href="https://web.whatsapp.com/send?phone={{ env('APP_Phone2') }}" target="_blank"><img
+    <a href="https://app.whatsapp.com/send?phone={{ env('APP_Phone2') }}" target="_blank"><img
             src="{{ asset('assets/images/whatsapp.png') }}" alt="whats app logo" width="50px" height="50px"></a>
 </div>
 
